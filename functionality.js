58e1488a4b5155bafecacd05dbed5eab90de0cb2
Awwlie <104977788+awwliedacoder@users.noreply.github.com>
2023-12-08 01:50:24 +0200

    Add files via upload

--- functionality.js
+++ functionality.js
@@ -1,51 +1,60 @@
-// Kanji ja Hiragana ja Katakana sanojen 'search engine'
-// Tarkoitus, että laitat sanan joko englanniksi tai japaniksi, ja se kääntää sanan APIN avulla.
-// Jisho.org:in tapainen sivusto...
+function searchDictionary() {
+    const apiKey = '5055871238mshd37c6a70d5be848p127251jsnb26bf4b67492'; 
+    const searchInput = document.getElementById('searchInput').value;
 
-
-// Otetaan muuttuja tekstilaatikosta, joka otetaan ja sitten laitetaan seuraavaksi tähän jos... 
-// Merkki on kanji, tai hiragana tai katana..
-//Katsotaan mitä kanji merkit / katakana / hiragana tarkoittaa
-let i = document.getElementsByClassName("TextArea");
-async function lookKanji() {
-    const url = `https://kanjialive-api.p.rapidapi.com/api/public/search/${i.toLowerCase()}`;
-    const options = {
-	    method: 'GET',
-	    headers: {
-		    'X-RapidAPI-Key': '5055871238mshd37c6a70d5be848p127251jsnb26bf4b67492',
-		    'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
-	}
-};
-    try {
-	    const response = await fetch(url, options);
-	    const result = await response.text();
-	    console.log(result);
-}   catch (error) {
-	    console.error(error);
-}
-}
-// Etsitään mahdollisia Kanjia englannikielisistä sanoista... Toimii näemmä vain pienillä kirjaimilla.
-async function searchKanji() {
-    const url = `https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?kem=${i.toLowerCase()}`;
+    // Katsotaan ettei inputti ole tyhjä.
+    if (!searchInput) {
+        alert('Please enter a kanji or word to search.');
+        return;
+    }
+    // Tehdään api requesti
+    const url = `https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?kem=${searchInput.toLowerCase()}`;
     const options = {
         method: 'GET',
+        
         headers: {
-            'X-RapidAPI-Key': '5055871238mshd37c6a70d5be848p127251jsnb26bf4b67492',
+            'X-RapidAPI-Key': apiKey,
             'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
         }
     };
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        // Handle the API response
+        displayResult(data);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        alert('An error occurred while fetching data. Please try again.');
+    });
 }
 
-function searchResult(){
-    const answer = ''
-    
+//Funktio joka näyttää sitten tämän vastauksen...
+      function displayResult(data) {
+            const resultContainer = document.getElementById('result');
+            
+            // Poistetaan vanhat vastaukset
+            resultContainer.innerHTML = '';
 
-}
+            if (data && data.length > 0) {
+                const meaning = document.getElementById('searchInput').value;
+                const meaning2 = meaning.charAt(0).toUpperCase() + meaning.slice(1);
+
+                // Vast
+                data.forEach(kanjiInfo => {
+                    const resultHTML = `
+                        <h2>${kanjiInfo.kanji.character}</h2>
+                        <p>Meaning: ${meaning2}</p>
+                        <p>Stroke Count: ${kanjiInfo.kanji.stroke}</p>
+                        <p>Radical: ${kanjiInfo.radical.character}</p>
+                        <p>Radical Stroke: ${kanjiInfo.radical.stroke}</p>
+                        <p>Radical order: ${kanjiInfo.radical.order}</p>
+
+                    `;
+                    resultContainer.innerHTML += resultHTML;
+                });
+            } else {
+                // Jos ei löydy vastauksia
+                resultContainer.innerHTML = '<p>No results found.</p>';
+            }
+        }
